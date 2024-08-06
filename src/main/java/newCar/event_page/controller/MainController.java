2f262fcfa020da137f65645ea726333745c2393f
bjh3311 <bjh3311@naver.com>
2024-08-06 17:56:05 +0900

    TASK-126 GET /main/event-time API 완성

--- src/main/java/newCar/event_page/controller/MainController.java
+++ src/main/java/newCar/event_page/controller/MainController.java
@@ -1,19 +1,23 @@
 package newCar.event_page.controller;
 
 import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.RequiredArgsConstructor;
 import newCar.event_page.model.dto.EventTimeDTO;
 import newCar.event_page.service.EventService;
 import newCar.event_page.service.QuizService;
 import newCar.event_page.service.RacingService;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("/main")
+@Tag(name = "Main API", description = "Main 이벤트 페이지 API 설계입니다")
 @RequiredArgsConstructor
+@CrossOrigin(origins = "*")
 public class MainController {
 
     private final EventService eventService;
@@ -21,7 +25,7 @@ public class MainController {
     private final RacingService racingService;
 
     @GetMapping("/event-time")
-    @Operation(summary = "이벤트 진행 기간 카운터")
+    @Operation(summary = "이벤트 진행 기간을 startTime, endTime 으로 반환한다")
     public ResponseEntity<EventTimeDTO> getEventTime(){
         return ResponseEntity.ok(eventService.getEventTime());
     }
