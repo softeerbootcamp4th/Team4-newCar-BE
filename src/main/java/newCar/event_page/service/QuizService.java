4ad8acaae571f077ad29964673400fdd69d3066a
bjh3311 <bjh3311@naver.com>
2024-08-06 20:00:52 +0900

    TASK-126 서울 시간을 ZoneId.of(Asia/Seoul)로 받아와서 오늘 날짜의 퀴즈를 찾아서 반환해준다

--- src/main/java/newCar/event_page/service/QuizService.java
+++ src/main/java/newCar/event_page/service/QuizService.java
@@ -2,6 +2,7 @@ package newCar.event_page.service;
 
 import lombok.RequiredArgsConstructor;
 import newCar.event_page.model.dto.QuizDTO;
+import newCar.event_page.model.dto.UserQuizDTO;
 import newCar.event_page.model.entity.event.Event;
 import newCar.event_page.model.entity.event.quiz.Quiz;
 import newCar.event_page.exception.UnmodifiableFieldException;
@@ -10,6 +11,8 @@ import newCar.event_page.repository.quiz.QuizRepository;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
+import java.time.LocalDate;
+import java.time.ZoneId;
 import java.util.List;
 import java.util.NoSuchElementException;
 
@@ -34,6 +37,18 @@ public class QuizService {
                 .map(QuizDTO::toDTO)
                 .limit(eventDuration)
                 .toList();
+    }
+
+    @Transactional(readOnly = true)
+    public UserQuizDTO getQuiz(Long quizEventId ){
+        Event quizEvent = eventRepository.findById(quizEventId)
+                .orElseThrow(() -> new NoSuchElementException("퀴즈 이벤트가 존재하지 않습니다."));
+
+        //LocalDate 한국 날짜를 기준으로 오늘의 퀴즈를 받아온다
+        Quiz todayQuiz = quizRepository.findByPostDate(LocalDate.now(ZoneId.of("Asia/Seoul")))
+                .orElseThrow(() -> new NoSuchElementException("오늘 날짜에 해당하는 퀴즈 이벤트가 존재하지 않습니다."));
+
+        return UserQuizDTO.toDTO(todayQuiz);
     }
 
     public QuizDTO updateQuiz(QuizDTO quizDTO) {
